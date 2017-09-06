b66e2a6f063b3dcd0cf6a0c16934ae5262c3cce5
Bryan Hunter <bdhunter3141@gmail.com>
2017-09-06 15:58:21 +0000

    created loop to add individual letters to object

--- count-letters.js
+++ count-letters.js
@@ -1,6 +1,14 @@
 const countLetters = function(str) {
   const lettersObj = {}
-  const splitStr = str.split("")
-  return splitStr
+  let splitStr = str.split("")
+  splitStr = splitStr.sort()
+  //Create loop to cycle through splitStr and add letter to lettersObj
+  for (let i = 0; i < splitStr.length; i++) {
+    if (splitStr[i] !== splitStr[i+1]) {
+      const theLetter = splitStr[i]
+      lettersObj[theLetter] = 1
+    }
+  }
+  return lettersObj
 }
 console.log(countLetters("alshgdsklajfkds"))
