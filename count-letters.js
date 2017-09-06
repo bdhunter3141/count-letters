7b8406c239293502ee742b495b68f0d96ad91818
Bryan Hunter <bdhunter3141@gmail.com>
2017-09-06 16:58:10 +0000

    will now count instances of letter and add to object

--- count-letters.js
+++ count-letters.js
@@ -1,12 +1,13 @@
 const countLetters = function(str) {
   const lettersObj = {}
   let splitStr = str.split("")
-  splitStr = splitStr.sort()
+  let sortStr = splitStr.sort().join("")
   //Create loop to cycle through splitStr and add letter to lettersObj
-  for (let i = 0; i < splitStr.length; i++) {
-    if (splitStr[i] !== splitStr[i+1]) {
-      const theLetter = splitStr[i]
-      lettersObj[theLetter] = 1
+  for (let i = 0; i < sortStr.length; i++) {
+    if (sortStr[i] !== sortStr[i+1]) {
+      const theLetter = sortStr[i]
+      const theNumber = str.split(sortStr[i]).length - 1
+      lettersObj[theLetter] = theNumber
     }
   }
   return lettersObj
